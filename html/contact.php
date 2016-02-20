58e28f085f64281c5e82f6e2dfefc3a84a1c0ca9
Aaron Stockdill <aaronstockdill@me.com>
2016-02-21 12:36:44 +1300

    Add social media links to contact page.

--- html/contact.php
+++ html/contact.php
@@ -9,6 +9,7 @@ include 'include/header.php';
             <div class='mini-menu'>
                 <h2>Contact</h2>
                 <a href='#details'>Details</a>
+                <a href='#social-media'>Social Media</a>
             </div>
 
             <div class='body'>
@@ -26,6 +27,16 @@ include 'include/header.php';
                         <p>For diversity-related enquiries, email <a href='mailto:equity@compsoc.org.nz'>equity@compsoc.org.nz</a>.</p>
 
                         <p>Etc...</p>
+                    </div>
+
+                </section>
+
+                <section id="details">
+                    <h3>Social Media</h3>
+
+                    <div class='text'>
+                        <p>Our Twitter is <a href='https://twitter.com/UCCompSoc' target='_blank'>@UcCompSoc</a></p>
+                        <p>Our Facebook page is <a href='https://www.facebook.com/UcCompSoc' target='_blank'>UcCompSoc</a></p>
                     </div>
 
                 </section>
