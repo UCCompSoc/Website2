61912738a64c1f0574723190724021b9719ed281
Aaron Stockdill <aaronstockdill@me.com>
2016-02-21 13:38:35 +1300

    Add study hack resources.

--- html/resources.php
+++ html/resources.php
@@ -8,36 +8,54 @@ include 'include/header.php';
 
             <div class='mini-menu'>
                 <h2>Resources</h2>
-                <a href='#heading'>Heading</a>
                 <a href='#stuff-and-things'>Stuff and Things</a>
             </div>
 
             <div class='body'>
-
-                <section id="heading">
-                    <h3>Heading</h3>
-
-                    <div class='text'>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-
-                </section>
 
                 <section id="stuff-and-things">
                     <h3>Stuff and Things</h3>
 
                     <ul class='resources'>
                         <li>
-                            <a href='#'><img src='http://placehold.it/100x100' />
-                            <span class='title'>Title</span></a>
-                            <span class='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+                            <a href='https://drive.google.com/a/compsoc.org.nz/file/d/0B5TVsqXFak7wZTBoMmhYUmxLRDg/view' target='_blank'><img src='/resources/hms.png' />
+                            <span class='title'>Class Note-taking</span></a>
+                            <span class='description'>Use this innovative note-taking technique that simultaneously facilitates understanding, increases readability and creates a framework for easy review.</span>
                             <div class='clearfix'></div>
                         </li>
 
                         <li>
-                            <a href='#'><img src='http://placehold.it/100x100' />
-                            <span class='title'>Title</span></a>
-                            <span class='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+                            <a href='https://drive.google.com/a/compsoc.org.nz/file/d/0B5TVsqXFak7wRXRILXZTcTdtU2M/view' target='_blank'><img src='/resources/hms.png' />
+                            <span class='title'>Memorizing Textbooks</span></a>
+                            <span class='description'>How do some students get A's in their classes without even showing up to class? They do it by effectively studying the course textbook &amp; materials. This tutorial will teach you how you can, too.</span>
+                            <div class='clearfix'></div>
+                        </li>
+
+                        <li>
+                            <a href='https://drive.google.com/a/compsoc.org.nz/file/d/0B5TVsqXFak7waUJEU1c4RFl4X28/view' target='_blank'><img src='/resources/hms.png' />
+                            <span class='title'>Focus and Concentration</span></a>
+                            <span class='description'>What do highly focused and productive people do differently? Learn how to dramatically increase your focus and concetration by creating a balanced sensory environment.</span>
+                            <div class='clearfix'></div>
+                        </li>
+
+                        <li>
+                            <a href='https://drive.google.com/a/compsoc.org.nz/file/d/0B5TVsqXFak7wSzFmLUNmRWJnV2M/view' target='_blank'><img src='/resources/hms.png' />
+                            <span class='title'>Stress Management</span></a>
+                            <span class='description'>Did you know that your body has many stress-fighting and happiness-inducing mechanisms that can be manually activated? Learn how to combat stress and anxiety with these tips and techniques.</span>
+                            <div class='clearfix'></div>
+                        </li>
+
+                        <li>
+                            <a href='https://drive.google.com/a/compsoc.org.nz/file/d/0B5TVsqXFak7wckdlSmFGaURKY2M/view' target='_blank'><img src='/resources/hms.png' />
+                            <span class='title'>Study Environment</span></a>
+                            <span class='description'>Do you find it easier to study at a cafe or library than at home? Learn how to change the lighting, surroundings and ergonomics of your study area for maximum focus and concentration.</span>
+                            <div class='clearfix'></div>
+                        </li>
+
+                        <li>
+                            <a href='https://drive.google.com/a/compsoc.org.nz/file/d/0B5TVsqXFak7wQUJGbncySWhJYWc/view' target='_blank'><img src='/resources/hms.png' />
+                            <span class='title'>Time Management</span></a>
+                            <span class='description'>What really separates the highly productive from the rest? Learn the differences between Classical and Modern students' time management philosophies, and how you may be doing the opposite of what you should.</span>
                             <div class='clearfix'></div>
                         </li>
                     </ul>
