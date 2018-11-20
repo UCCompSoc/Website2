3b36077e706f6d3802b9df48c198e990085e7f88
Jake Faulkner <jakefaulkn@gmail.com>
2018-11-20 19:43:57 +1300

    feat: migrate core web files from php to flask

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
+
+@app.route('/sponsors')
+def sponsors():
+    return render_template('sponsors.html')
+
+@app.route('/events-loader')
+def event_loader():
+    auth_token = fetch_auth_token()
+    events = fetch_events(auth_token)
+    return jsonify(events)
+
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
