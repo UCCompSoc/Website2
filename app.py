024e77cf98d7c203c529d47260c1e8e7076ab353
Jake Faulkner <jakefaulkn@gmail.com>
2018-11-22 16:07:50 +1300

    feat: respect do not track for Google Analytics

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import requests
 
 app = Flask(__name__)
+DNT_HEADERS = ['Dnt', 'X-Do-Not-Track', 'DNT']
+
+def dnt_enabled():
+    ''' Returns True if the client has set the DNT header. '''
+    return any(request.headers.get(header, None) == '1' for header in DNT_HEADERS)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', dnt=dnt_enabled())
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', dnt=dnt_enabled())
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', dnt=dnt_enabled())
 
 @app.route('/resources')
 def resources():
-    return render_template('resources.html')
+    return render_template('resources.html', dnt=dnt_enabled())
 
 @app.route('/sponsors')
 def sponsors():
-    return render_template('sponsors.html')
+    return render_template('sponsors.html', dnt=dnt_enabled())
 
 @app.route('/events')
 def events():
-    return render_template('events.html')
+    return render_template('events.html', dnt=dnt_enabled())
 
 if __name__ == '__main__':
     app.run()
